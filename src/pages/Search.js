2de2f6910bec3644f6c53ba1ceec9b7c3650994d
julianjohannesen <github email address>
2019-08-04 12:44:45 -0400

    added layout dir + Header & Footer, also added those to pages

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Header from '../layout/Header';
+import Footer from '../layout/Footer';
 
 export default function Search() {
     return (
         <div>
-            
+            <Header />
+            <Footer />
         </div>
     )
 }
