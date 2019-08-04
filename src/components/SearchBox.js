b9870dc6d8ecb2ac2e86ec1be9b99b39328e237a
julianjohannesen <github email address>
2019-08-04 12:40:34 -0400

    added pages and components, empty for now

---
+++ src/components/SearchBox.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SearchBox extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
