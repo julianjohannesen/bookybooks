490fccb05c974bc8483d62bb04925c4a68969d50
julianjohannesen <github email address>
2019-09-17 12:31:38 -0400

    move some state setting into componentDidUpdate

--- src/components/OAuth2.js
+++ src/components/OAuth2.js
@@ -10,7 +10,7 @@ export default class OAuth2 extends Component {
         isAuthorized: false
     }
 
-    // Handle load, or load errors
+    // Handle loading gapi, or handle any errors
     loadCallbackConfig = {
         callback: () => { initClient },
         onerror: () => { throw new Error() },
@@ -31,33 +31,20 @@ export default class OAuth2 extends Component {
     }
 
     initClient() {
-        console.log("Is gapi.client.init defined?", window.gapi.client.init)
         window.gapi.client.init(this.authDetails)
-            .then(function () {
+            .then( () => {
                 this.setState({
                     GoogleAuth: window.gapi.auth2.getAuthInstance()
-                })
-            })
-            .then(function () {
-                this.setState({
-                    user: this.state.GoogleAuth.currentUser.get(),
-                    isSignedIn: this.state.GoogleAuth.isSignedIn.get(),
-                    isAuthorized: this.state.user.hasGrantedScopes(this.scope)
-                })
-            })
-            .then(function () {
-                this.state.GoogleAuth.isSignedIn.listen(this.setSigninStatus);
-            })    
+                });
+            });
         console.log("The state at the end of initClient: ", this.state)
-    }
-        
+    }            
     
-
     handleAuthClick(isSignedIn) {
         if (isSignedIn) { this.state.GoogleAuth.signOut() }
         else { this.state.GoogleAuth.signIn() }
     }
-
+    
     setSigninStatus() {
         this.setState({
             user: this.state.GoogleAuth.currentUser.get(),
@@ -67,27 +54,46 @@ export default class OAuth2 extends Component {
         if (this.state.isAuthorized) {
             document.querySelector('#sign-in').textContent = 'Sign out';
             document.querySelector('#revoke-access').style.display = 'inline-block';
-
+            
         } else {
             document.querySelector('#sign-in').textContent = 'Sign In';
             document.querySelector('#revoke-access').style.display = 'none';
         }
     }
-
+    
     revokeAccess() { this.state.GoogleAuth.disconnect(); }
-
+    
     componentDidMount() {
+        // Once the component has mounted, start the authorization process
         window.gapi.load('client:auth2', this.loadCallbackConfig);
-        document.getElementById('sign-in').onclick = () => this.handleAuthClick(this.state.isSignedIn);
     }
-
-    render() {
-
-        return (
-            <div>
+    
+    componentDidUpdate(preProps, preState) {
+        // Once the Google Authorization Object is stored in state, determine whether a user is signed in
+        if(this.state.GoogleAuth !== prevState.GoogleAuth){
+            this.setState({
+                isSignedIn: this.state.GoogleAuth.isSignedIn.get()
+            });
+            document.getElementById('sign-in').addEventListener( () => this.handleAuthClick(this.state.isSignedIn) );
+        }
+        // Once a user has signed in, get the current user and determine authorization
+        if(this.state.isSignedIn !== prevState.isSignedIn){
+            this.setState({
+                user: this.state.GoogleAuth.currentUser.get(),
+                isAuthorized: this.state.user.hasGrantedScopes(this.scope)
+            });
+            // Once a user has signed in, start listening for changes to sign in status
+            this.state.GoogleAuth.isSignedIn.listen(this.setSigninStatus);
+        }
+    }
+        
+        render() {
+            
+            return (
+                <div>
                 <button className={cn('button', 'primary')} id="sign-in">Sign In</button>
                 <button className={cn('button', 'primary')} id="revoke-access" style={{ "display": "none" }} onClick={this.revokeAccess}>Revoke access</button>
-            </div>
-        )
-    }
-}
+                </div>
+                )
+            }
+        }
