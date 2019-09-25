853c4c83464f7a4faad673cf3bf30155f6c0951c
julianjohannesen <github email address>
2019-09-25 19:50:16 -0400

    Trying out privacy adn terms comps, and about and contact

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
 	return (
 		<footer>
 			<div className="content has-text-centered">
-				<p><strong>BookyBooks</strong> by <a href="https://github.com/julianjohannesen">Julian Johannesen</a>. The source code is licensed under <a href="http://opensource.org/licenses/mit-license.php">MIT License</a>.</p>
+				<p className="is-marginless"><strong>BookyBooks</strong> by <a href="https://github.com/julianjohannesen">Julian Johannesen</a>. Source code licensed under <a href="http://opensource.org/licenses/mit-license.php">MIT License</a>.</p>
+				<p><Link to='/terms'>Terms</Link> and <Link to='/privacy'>Privacy</Link> policy.</p>
+				<p></p>
 			</div>
 		</footer>
 	)
