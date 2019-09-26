a45140f165f7871cba526eacea141e61db4e7f5c
julianjohannesen <github email address>
2019-09-26 14:22:01 -0400

    re-activated the typography modules in Head component

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import Helmet from 'react-helmet';
 // Typography doesn't play well with CRA, but using the React helper components still seems to work. See https://github.com/KyleAMathews/typography.js/issues/177
-//import typography from '../utils/Typography';
-//import { TypographyStyle, GoogleFont } from 'react-typography'
+import typography from '../utils/Typography';
+import { TypographyStyle, GoogleFont } from 'react-typography'
 
 export default class Head extends Component {
     render() {
-        // <TypographyStyle typography={typography} />
+        <TypographyStyle typography={typography} />
         // {/* This will appear in the body */}
-        // <GoogleFont typography={typography} />
+        <GoogleFont typography={typography} />
         return (
             <div>
                 
